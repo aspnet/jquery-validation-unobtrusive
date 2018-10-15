d7e4dbe829416d3411798f71242beb86e2055de5
Javier Calvarro Nelson <jacalvar@microsoft.com>
2018-10-15 11:08:26 -0700

    tmp

---
+++ test/Websites/ClientValidationWebSite/Controllers/ValidationController.cs
@@ -0,0 +1,21 @@
+﻿using ClientValidationWebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientValidationWebSite.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class ValidationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Validate([FromForm] ValidationModel model)
+        {
+            return RedirectToAction("Error", "Home");
+        }
+    }
+}
---
+++ test/Websites/ClientValidationWebSite/Models/ValidationModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientValidationWebSite.Models
+{
+    public class ValidationModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
